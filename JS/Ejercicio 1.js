eb676d74f64cf5fb5b420180ad4341dfed462f3f
Jinson25 <am987406@gmail.com>
2022-02-23 08:10:53 -0500

    Estructura basica del proyecto

---
+++ JS/Ejercicio 1.js
@@ -0,0 +1,7 @@
+document.getElementById("tipo1").innerHTML = "Ejemplo del tipo 1 del Output document.getElementById(ID).innerHTML";
+
+document.write("Ejemplo de texto con document.write(texto) solo se usa para pruebas");
+
+window.alert("Ejemplo de output con window.alert");
+
+console.log("Hola Mundo");
